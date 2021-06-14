195a3503a4c3ad5ecfabb5ad8b6458ab9f8e29ad
FranciscoLlabres <franciscollabres14@gmail.com>
2021-06-14 16:33:47 -0300

    algo

--- misfunciones.js
+++ misfunciones.js
@@ -1,5 +1,5 @@
 
-function check(){
+function check1(){
     var checkBox = document.getElementById("Check1");
     var canvas = document.getElementById("micanvas");
     var ctx = canvas.getContext("2d");
@@ -7,8 +7,13 @@ function check(){
     img.src = "Imagenes/hospital.jpg";
 
 
-    if (checkBox.checked = true)
+
+    if (checkBox.checked === true)
     {
         ctx.drawImage(img, 0, 0);
+    }
+    if (checkBox.checked === false)
+    {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 }
