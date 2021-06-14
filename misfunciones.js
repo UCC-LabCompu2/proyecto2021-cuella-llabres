a9e506b3673211514f6ed5633d6de76cbbdc16ca
FranciscoLlabres <franciscollabres14@gmail.com>
2021-06-14 16:23:21 -0300

    Cambios

--- misfunciones.js
+++ misfunciones.js
@@ -1,10 +1,10 @@
+
 function check(){
     var checkBox = document.getElementById("Check1");
     var canvas = document.getElementById("micanvas");
     var ctx = canvas.getContext("2d");
     var img = new Image();
     img.src = "Imagenes/hospital.jpg";
-
 
 
     if (checkBox.checked = true)
