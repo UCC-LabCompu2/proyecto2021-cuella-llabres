36a1f20c92289d2f463fcdd432f7bcc5c06d4a21
FranciscoLlabres <franciscollabres14@gmail.com>
2021-06-15 15:29:00 -0300

    Cambioosssss

--- misfunciones.js
+++ misfunciones.js
@@ -1,7 +1,10 @@
 function check1() {
-    var checkBox = document.getElementById("Check1");
     let canvas = document.getElementById("micanvas");
+    var val = document.getElementById("cantidaddesintomas").value;
     var ctx = canvas.getContext("2d");
+    if (val != 1 && val != 2 && val != 3 && val != 4 && val != 5 && val != 6 && val != 7 && val != 8 && val != 9) {
+        alert("Solo se permiten números o numeros entre el 1 y el 9");
+    }
     ctx.beginPath();
     ctx.arc(100, 75, 50, 0, 2 * Math.PI);
     ctx.stroke();
