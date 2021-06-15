1bfbf09d0ed5792d6ad94b8066c0cdab39ece906
FranciscoLlabres <franciscollabres14@gmail.com>
2021-06-15 14:38:36 -0300

    Nuevo modelo

--- misfunciones.js
+++ misfunciones.js
@@ -4,13 +4,6 @@ function check1() {
     var ctx = canvas.getContext("2d");
     ctx.beginPath();
     ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-
-
-    if (checkBox.checked === true) {
-        ctx.stroke();
-    } else (checkBox.checked === false)
-    {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    }
+    ctx.stroke();
 }
 
