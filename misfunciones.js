0417c71c5ff8bbe80fd4c7cf2ed6391c3be85edb
FranciscoLlabres <franciscollabres14@gmail.com>
2021-06-15 22:41:54 -0300

    Ultimos cambios

--- misfunciones.js
+++ misfunciones.js
@@ -16,13 +16,13 @@ function check1() {
     var valorm = document.getElementById("sintl").value;
     var ctx = myCanvas.getContext("2d");
 
-    if (valorg <= 0 || valorg >= 4) {
+    if (valorg != 1 && valorg != 2 && valorg != 3) {
         alert("Elija valores solamente entre el 1 y el 3");
     }
     if (val != 1 && val != 2 && val != 3) {
         alert("Solo se permiten números entre el 1 y el 3");
     }
-    if (valorm <= 0 || valorm >= 8) {
+    if (valorm != 1 && valorm != 2 && valorm != 3 && valorm != 4 && valorm != 5 && valorm != 6 && valorm != 7) {
         alert("Solo se permiten números entre el 1 y el 7")
     }
     if (val >= 1 && val <= 3 && valorg >= 1 && valorg <= 3 && valorm >= 1 && valorm <= 7) {
